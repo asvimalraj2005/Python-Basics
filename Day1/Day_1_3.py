e9e37cc43054f9e70e54d148c5bd055f3f86bcdd
asvimalraj2005 <asvimalraj2005@gmail.com>
2025-05-18 19:48:05 +0530

    Python Dynamic Function

--- Day1/Day_1_3.py
+++ Day1/Day_1_3.py
@@ -8,3 +8,11 @@ Addition = lambda A,B: A+B
 print(Addition(4,4))
 print(Addition(5,5))
 print(Addition(10,10))
+
+# Lambda function assigned to variable twice
+twice = lambda A : A * 2
+print(twice(10))
+
+# Lambda function not assigned ti bay variable twice
+print((lambda A : A*2)(8))
+
