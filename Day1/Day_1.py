3c9e4841d717cae465f1de2b347abd694c244f64
Vimal Raj <146108871+asvimalraj2005@users.noreply.github.com>
2025-05-18 18:34:53 +0530

    Update Day_1.py

---
+++ Day1/Day_1.py
@@ -0,0 +1,32 @@
+def ReservedWords():
+    pass
+# and  assert break  class continue def del elif else except exec finally 
+# for from global if import in is lambda pass print raise return try while with yield 
+# The above words will have different use-cases and purpose 
+# 
+#
+# and is an keyword used to seperate two conditions and it will used to check the both the condition (in general ) and in other cases it is used for assigning values, executing two function ect 
+# assert is used for debugging purposes it test whether the condition is true if not it raises an AssertionError
+# break is exit from the loop when the condition gets satisfied
+# continue is used to skip the current value inside the range of an loop
+# def is an keyword used to define the function 
+# del is used to delete the object or an value present inside any of an data_structure
+# elif is used to create many condition inbetween if-else statement 
+# except is used in error handling, try block executes the code, if code has any errors then except block will get executed 
+# exec is also an reserved keyword used for dynamic program code implementation where the syntax of it can be laid as exec(object,[,globals[,locals]])
+# finally is used in handling exception where try and except block will be executed and atlast mandatorily the finally block of it will get executed too 
+# for is used to initialise the loop
+# from is an keyword used to import python libraries to an file or modules from the library
+# if is used in the if-else conditional statements 
+# import is used to import the modules or library function from other the python environment 
+# in is an keyword whether the current element which is stored in an variable named A is present inside the data structure or not if it is present it returns True if not it returns False 
+# 'is' is also an reserved keyword where to check if any two object is compared between the object values if it same it will return them as True if not it will be returned as False but in some cases the values are least compared only the object is compared 
+# lambda keyword is used to create anonymous functions in a single line; syntax for it 'lambda arguments : expression' 
+# pass is an keyword used to override the fucntion without doing anything inside and calling them over by using objects 
+# print is used to print the statements
+# raise is an keyword used to raise pre-defined errors when the code in the try block contains error in them 
+# return is used to return the calculated value of the function to the main function 
+# try is used in error handling where the code to get executed is placed here 
+# while is an reserved keyword used for initialising loops over a range 
+# with is an reserved keyword used for start and stop of the statements in an dynamic and automatic manners 
+# yield is to pass the function and return the value to the main function after returning the function will resume it's work 
