c12e89cc6c76c730a0517b03fad98bcf42f484c9
asvimalraj2005 <asvimalraj2005@gmail.com>
2025-05-20 19:16:34 +0530

    String to num ; Num to string

---
+++ Day2/String2Num.py
@@ -0,0 +1,16 @@
+# The ord() function returns the ASCII code of the character and the chr() function returns character represented by a ASCII number
+#
+# ORD() and CHR() are mainly used in DSA for conversion purposes 
+#
+# Below is the example where we are converting string character into number
+ch='C'
+print(ord(ch))
+#
+#
+# Below is the example where we are converting ASCII value which is a number that is associated with the character 
+print(chr(65))
+#
+Alphabet=65
+print(chr(Alphabet))
+
+
