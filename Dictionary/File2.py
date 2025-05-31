f59b49b059a50e1b3eedb0a4b65d0b3104c2780e
Vimal Raj <146108871+asvimalraj2005@users.noreply.github.com>
2025-05-31 11:46:21 +0530

    File2.py

---
+++ Dictionary/File2.py
@@ -0,0 +1,9 @@
+# Built in dictionary functions
+# len(dict)   --> Returns the length of the dictionary
+# str(dict)   --> Returns the string representation of the dictionary
+# dict.clear() --> removes every entry in the dictionary 
+# dict.copy()  --> Returns an new dictionary with an original reference to the original dict
+# dict.get(key)--> The passed key will be used to retrieve the value from the dictionary 
+# dict.values() --> return the key value pairs of the dictionary in an tuple fornat 
+# dict.keys() --> returns the keys which are present in the dictionary 
+# dict.values() --> returns the values which are present in the dictionary 
